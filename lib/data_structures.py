75ad25890065120b05b92fbe8b77da1397c65a30
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-08-16 18:57:13 +0300

    initial commit

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,40 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
+    for food in spicy_foods:
+        print(food["name"])
     pass
 
+
 def get_spiciest_foods(spicy_foods):
+    spiciest_foods = []
+    for food in spicy_foods:
+        if food.get("heat_level", 0) > 5:
+            spiciest_foods.append(food)
+    return spiciest_foods
     pass
+
+
+get_spiciest_foods(spicy_foods)
+
 
 def print_spicy_foods(spicy_foods):
     pass
 
+
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
+
 
 def print_spiciest_foods(spicy_foods):
     pass
 
+
 def get_average_heat_level(spicy_foods):
     pass
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
